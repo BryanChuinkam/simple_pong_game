ed4446821ebbaab1f8fda1b3fd5b8de68e310078
bchuinkam <bryan.chuinkam@wisewiithdata.com>
2021-03-04 08:29:15 -0500

    Added feature: - ball speeds up after each paddle hit. - ball speed resents to initial speed after a player scores.

--- pong_game.py
+++ pong_game.py
@@ -1,5 +1,4 @@
 # simple Pong in Python 3 for Beginners
-# TODO Make ball gradually go faster while playing.
 # TODO Add sound effects from https://freesound.org/
 
 import turtle
@@ -10,6 +9,7 @@ wn.title("Pong for Caleb")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)		# size of window
 wn.tracer(0)						# stops window from updating. Hence speeding up game
+initial_speed = 0.025
 
 # prompt players for their name
 player_one = turtle.textinput("Enter First Player name", "Player one: ")
@@ -39,13 +39,13 @@ paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(0)					# speed of animation
+ball.speed(0)					# speed of animation NOT speed of ball
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.025
-ball.dy = 0.025
+ball.dx = initial_speed
+ball.dy = initial_speed
 
 # Pen
 pen = turtle.Turtle()
@@ -56,35 +56,35 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("{}: 0  {}: 0".format(player_one, player_two), align="center", font=("Courier", 24, "normal"))
 
-# Function
+# Functions
 
 
 def paddle_a_up():
 	""" moves paddle_a"""
 	y = paddle_a.ycor()			# get y coordinates of paddle_a
-	y += 20						# move it up by 20 pixles
-	paddle_a.sety(y)			# set new y coordiante
+	y += 20						# move it up by 20 pixels
+	paddle_a.sety(y)			# set new y coordinate
 
 
 def paddle_a_down():
 	""" moves paddle_a"""
 	y = paddle_a.ycor()			# get y coordinates of paddle_a
-	y += -20						# move it down by 20 pixles
-	paddle_a.sety(y)			# set new y coordiante
+	y += -20						# move it down by 20 pixels
+	paddle_a.sety(y)			# set new y coordinate
 
 
 def paddle_b_up():
 	""" moves paddle_b"""
 	y = paddle_b.ycor()			# get y coordinates of paddle_b
-	y += 20						# move it up by 20 pixles
-	paddle_b.sety(y)			# set new y coordiante
+	y += 20						# move it up by 20 pixels
+	paddle_b.sety(y)			# set new y coordinate
 
 
 def paddle_b_down():
 	""" moves paddle_b"""
 	y = paddle_b.ycor()			# get y coordinates of paddle_b
-	y += -20					# move it down by 20 pixles
-	paddle_b.sety(y)			# set new y coordiante
+	y += -20					# move it down by 20 pixels
+	paddle_b.sety(y)			# set new y coordinate
 
 # keyboard binding
 
@@ -117,25 +117,28 @@ while True:
 
 	if ball.xcor() > 390:
 		ball.goto(0, 0)
-		ball.dx *= -1
+		ball.dx = initial_speed  # reset speed after a player scores
 		score_a += 1
 		pen.clear()
 		pen.write("{}: {}  {}: {}".format(player_one, score_a, player_two, score_b), align="center", font=("Courier", 24, "normal"))
 
 	if ball.xcor() < -390:
 		ball.goto(0, 0)
+		ball.dx = initial_speed  # reset speed after a player scores
 		ball.dx *= -1
 		score_b += 1
 		pen.clear()
 		pen.write("{}: {}  {}: {}".format(player_one, score_a, player_two, score_b), align="center", font=("Courier", 24, "normal"))
 
 	# paddle and ball collisions
-	if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+	if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 60 and ball.ycor() > paddle_b.ycor() - 60):
 		ball.setx(340)
 		ball.dx *= -1
+		ball.dx -= 0.025  # speed it up
 		# os.system("aplay bounce.wav&")
 
-	if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+	if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 60 and ball.ycor() > paddle_a.ycor() - 60):
 		ball.setx(-340)
 		ball.dx *= -1
+		ball.dx += 0.025  # speed it up
 		# os.system("aplay bounce.wav&")
